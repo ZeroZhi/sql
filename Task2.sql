2c6f1b568a710d8ca608f1c109ca02fc4ec13547
Zeka Zilin <ZeroZhi@users.noreply.github.com>
2015-05-26 11:06:31 +0300

    Update HW6 part 2

--- Task2.sql
+++ Task2.sql
@@ -2,7 +2,7 @@ SELECT city.name AS super_city, region.name AS super_area
 FROM city LEFT JOIN region ON region.id = city.region_id 
 WHERE city.id >=300
 AND city.id <=1000
-AND (city.name LIKE 'À%' 
-  OR city.name LIKE 'Î%' 
-  OR city.name LIKE 'Ò%' 
-  OR city.name LIKE 'Ó%')
+AND (city.name LIKE 'Ð%' 
+  OR city.name LIKE 'Ðž%' 
+  OR city.name LIKE 'Ð¢%' 
+  OR city.name LIKE 'Ð£%')
